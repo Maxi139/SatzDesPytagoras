87755c41151d7c02d938138c6714087421d259a4
Maxi139 <duckify6@gmail.com>
2024-09-30 16:43:24 +0200

    fsa

--- script.js
+++ script.js
@@ -3,38 +3,36 @@ var b = document.getElementById("b");
 var c = document.getElementById("c");
 var button = document.getElementById("button");
 
+//FIXME: Fix it
 function pythagoras(a, b, c) {
-    var c = Math.sqrt(a * a + b * b);
-    c = c.toFixed(2);
-    console.log(c);
-    return c;
+  var c = Math.sqrt(a * a + b * b);
+  c = c.toFixed(2);
+  console.log(c);
+  return c;
 }
 
-function update(get){
-    if (get) {
-        a = document.getElementById("a").value;
-        b = document.getElementById("b").value;
-        c = document.getElementById("c").value;
-    }else{
-        document.getElementById("a").value = a;
-        document.getElementById("b").value = b;
-        document.getElementById("c").value = c;
-    }
+function update(get) {
+  if (get) {
+    a = document.getElementById("a").value;
+    b = document.getElementById("b").value;
+    c = document.getElementById("c").value;
+  } else {
+    document.getElementById("a").value = a;
+    document.getElementById("b").value = b;
+    document.getElementById("c").value = c;
+  }
 }
 
-button.addEventListener("click", function() {
-    update(true);
-    if(a && b){
-        c = pythagoras(parseFloat(a), parseFloat(b), parseFloat(c));
-        update(false);
-    }
-    else if(a && c){
-        b = pythagoras(parseFloat(c), parseFloat(a * -1), parseFloat(b));
-        update(false);
-    }
-    else if(b && c){
-        a = pythagoras(parseFloat(c), parseFloat(b*-1), parseFloat(c));
-        update(false);
-    }
-}) 
-
+button.addEventListener("click", function () {
+  update(true);
+  if (a && b) {
+    c = pythagoras(parseFloat(a), parseFloat(b), parseFloat(c));
+    update(false);
+  } else if (a && c) {
+    b = pythagoras(parseFloat(c), parseFloat(a * -1), parseFloat(b));
+    update(false);
+  } else if (b && c) {
+    a = pythagoras(parseFloat(c), parseFloat(b * -1), parseFloat(c));
+    update(false);
+  }
+});
